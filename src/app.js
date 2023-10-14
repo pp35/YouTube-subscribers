f1455c1a3afa6ddf0a6c8976c620fc4231e159b0
pp35 <poornimapandey3590@gmail.com>
2023-10-15 01:59:40 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -1,45 +1,49 @@
-const express = require("express");
+// src/app.js
+
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-
-const { port, dbURI } = require("./index");
-
-// Connect to MongoDB
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
-
-// Define the Mongoose model for Subscribers
-const subscriberSchema = new mongoose.Schema({
-  name: String,
-});
-
-const Subscriber = mongoose.model("Subscriber", subscriberSchema);
+const Subscriber = require('./models/subscribers');
 
 // Middleware to parse JSON requests
 app.use(express.json());
-
-// Replace this with your subscriber data
-const subscribers = [
-  { name: "Subscriber 1" },
-  { name: "Subscriber 2" },
-  { name: "Subscriber 3" },
-];
-
-// Get all subscribers
-app.get("/subscribers", (req, res) => {
-  res.json(subscribers); // Respond with the subscribers array as JSON
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-// Create a new subscriber and save it to the database
-app.post("/subscribers", async (req, res) => {
+// Route to get all subscribers
+app.get('/subscribers', async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+app.post('/subscribers', async (req, res) => {
+  try {
+    const newSubscriber = new Subscriber(req.body);
+    const savedSubscriber = await newSubscriber.save();
+    res.status(201).json(savedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+app.get('/subscribers/name', async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find().select('name subscribedChannel -_id');
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.post('/subscribers/name', async (req, res) => {
   const subscriber = new Subscriber({
     name: req.body.name,
+    subscribedChannel: req.body.subscribedChannel
   });
+
   try {
     const newSubscriber = await subscriber.save();
     res.status(201).json(newSubscriber);
@@ -47,9 +51,39 @@ app.post("/subscribers", async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get('/subscribers/:id', async (req, res) => {
+  try {
+    const subscriber = await Subscriber.findById(req.params.id);
+    if (!subscriber) {
+      return res.status(400).json({ message: 'Subscriber not found' });
+    }
+    res.json(subscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
+
+app.post('/subscribers/:id', async (req, res) => {
+  const { name, subscribedChannel } = req.body;
+
+  if (!name || !subscribedChannel) {
+    return res.status(400).json({ message: 'Both name and subscribedChannel are required' });
+  }
+
+  const newSubscriber = new Subscriber({
+    name,
+    subscribedChannel
+  });
+
+  try {
+    const savedSubscriber = await newSubscriber.save();
+    res.status(201).json(savedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
+// Middleware function to get a subscriber by ID
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = require('./app.js')
 const mongoose = require('mongoose')
@@ -9,22 +8,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
-const DATABASE_URL = process.env.MONGODB_URL || "mongodb://localhost/subscribers";
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-
+const DATABASE_URL = "mongodb://localhost/subscribers";
+mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
-db.on('open', () => {
-    // The connection is successful; you can check the existence of your database here.
-    const databaseName = 'subscribers'; // Replace with your database name
-    mongoose.connection.db.listCollections({ name: databaseName }).next((err, collinfo) => {
-      if (collinfo) {
-        console.log(`Database '${databaseName}' exists.`);
-      } else {
-        console.log(`Database '${databaseName}' does not exist.`);
-      }
-    });
-  });
-  
-  // Start Server
+db.once('open', () => console.log('connected to database'))
+
+// Start Server
 app.listen(port, () => console.log(`App listening on port ${port}!`))
