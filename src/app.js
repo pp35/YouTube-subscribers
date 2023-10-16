037fe4ff6ac9d917ba76749d4375e1fd60051370
pp35 <poornimapandey3590@gmail.com>
2023-10-17 02:28:55 +0530

    tg

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
-// src/app.js
-
+// Importing necessary modules
 const express = require('express');
 const app = express();
 const Subscriber = require('./models/subscribers');
 
 // Middleware to parse JSON requests
 app.use(express.json());
+
+// Serving static files from the current directory
 app.use(express.static(__dirname));
+
+// Serving the index.html file when the root URL is accessed
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -20,6 +23,8 @@ app.get('/subscribers', async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+// Creating a new subscriber
 app.post('/subscribers', async (req, res) => {
   try {
     const newSubscriber = new Subscriber(req.body);
@@ -29,6 +34,8 @@ app.post('/subscribers', async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+
+// Retrieving subscribers with only name and subscribedChannel fields
 app.get('/subscribers/name', async (req, res) => {
   try {
     const subscribers = await Subscriber.find().select('name subscribedChannel -_id');
@@ -38,6 +45,7 @@ app.get('/subscribers/name', async (req, res) => {
   }
 });
 
+// Creating a new subscriber with only name and subscribedChannel fields
 app.post('/subscribers/name', async (req, res) => {
   const subscriber = new Subscriber({
     name: req.body.name,
@@ -51,18 +59,21 @@ app.post('/subscribers/name', async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+
+// Retrieving a subscriber by their ID
 app.get('/subscribers/:id', async (req, res) => {
   try {
     const subscriber = await Subscriber.findById(req.params.id);
     if (!subscriber) {
-      return res.status(400).json({ message: 'Subscriber not found' });
+      return res.status(404).json({ message: 'Subscriber not found' });
     }
     res.json(subscriber);
   } catch (err) {
-    res.status(400).json({ message: 'Subscriber not found' });
+    res.status(400).json({ message: 'Invalid ID format' });
   }
 });
 
+// Creating a new subscriber with provided name and subscribedChannel, given an ID
 app.post('/subscribers/:id', async (req, res) => {
   const { name, subscribedChannel } = req.body;
 
@@ -83,7 +94,18 @@ app.post('/subscribers/:id', async (req, res) => {
   }
 });
 
-
 // Middleware function to get a subscriber by ID
+app.param('id', async (req, res, next, id) => {
+  try {
+    const subscriber = await Subscriber.findById(id);
+    if (!subscriber) {
+      return res.status(404).json({ message: 'Subscriber not found' });
+    }
+    req.subscriber = subscriber;
+    next();
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = app;
